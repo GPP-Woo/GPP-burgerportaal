db1862d72402ebf6ae297aa0de7e50b648abb439
Felix Cornelissen <felix.cornelissen@icatt.nl>
2025-02-24 15:34:26 +0100

    fix issues with weird dates

--- odbp.client/src/helpers/date.ts
+++ odbp.client/src/helpers/date.ts
@@ -2,14 +2,16 @@ type DateLike = string | null | undefined | Date;
 
 const nlLongFormat = Intl.DateTimeFormat("nl-NL", { dateStyle: "long" });
 
-const isoDateFormat = Intl.DateTimeFormat("sv-SE", {
-  year: "numeric",
-  day: "2-digit",
-  month: "2-digit"
-});
+const padZero = (v: string | number, count: number) => {
+  v = v.toString();
+  while (v.length < count) {
+    v = "0" + v;
+  }
+  return v;
+};
 
 const parseValidDate = (date: DateLike) => {
-  if (!date) return null;
+  if (!date) return undefined;
   date = new Date(date);
   if (date instanceof Date && !isNaN(date.getTime())) return date;
   return date;
@@ -23,7 +25,11 @@ export const formatDate = (date: DateLike) => {
 
 export const formatIsoDate = (date: DateLike) => {
   date = parseValidDate(date);
-  return date && isoDateFormat.format(date);
+  if (!date) return undefined;
+  const year = padZero(date.getFullYear(), 4),
+    month = padZero(date.getMonth() + 1, 2),
+    day = padZero(date.getDate(), 2);
+  return [year, month, day].join("-");
 };
 
 export const addToDate = (
@@ -31,7 +37,7 @@ export const addToDate = (
   addition: { year?: number; month?: number; day?: number }
 ) => {
   d = parseValidDate(d);
-  if (!d) return null;
+  if (!d) return undefined;
   let year = d.getFullYear();
   let month = d.getMonth();
   let day = d.getDate();
