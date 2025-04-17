97d063676fe04e527c420a32fd99162d21791df1
nijmra <armijn@gmail.com>
2025-04-17 11:15:06 +0200

    spotlight style refactor

--- odbp.client/src/features/onderwerp/OnderwerpenOverview.vue
+++ odbp.client/src/features/onderwerp/OnderwerpenOverview.vue
@@ -76,6 +76,13 @@ const showButton = computed(() => onderwerpen.value?.length !== lijsten.value?.o
 
 <style lang="scss" scoped>
 .utrecht-spotlight-section {
+  --utrecht-spotlight-section-padding-inline-start: var(--utrecht-page-margin-inline-start);
+  --utrecht-spotlight-section-padding-inline-end: var(--utrecht-page-margin-inline-end);
+  --utrecht-spotlight-section-padding-block-start: var(--utrecht-page-padding-block-start);
+  --utrecht-spotlight-section-padding-block-end: var(
+    --utrecht-spotlight-section-padding-block-start
+  );
+
   margin-inline-end: calc(-1 * var(--utrecht-page-margin-inline-end));
   margin-inline-start: calc(-1 * var(--utrecht-page-margin-inline-start));
 
