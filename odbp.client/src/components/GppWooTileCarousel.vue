2f59b59bfa0050aff5c8aeec4f6f40bcfd3227c6
nijmra <armijn@gmail.com>
2025-04-17 12:14:14 +0200

    setup tile carousel
    setup tile carousel

---
+++ odbp.client/src/components/GppWooTileCarousel.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="(tile, index) in tiles" :key="index">
+      <gpp-woo-tile v-bind="tile" />
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import GppWooTile, { type Tile } from "@/components/GppWooTile.vue";
+
+defineProps<{ tiles: Tile[] }>();
+</script>
+
+<style lang="scss" scoped>
+
+</style>
