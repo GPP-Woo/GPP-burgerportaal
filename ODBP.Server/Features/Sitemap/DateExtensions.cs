eca3953afb44163babc07baa2a5093e645df2fa8
Felix Cornelissen <felix.cornelissen@icatt.nl>
2025-06-26 10:28:13 +0200

    documenthandelingen in sitemap obv datumvelden

---
+++ ODBP.Server/Features/Sitemap/DateExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace ODBP.Features.Sitemap
+{
+    public static class DateExtensions
+    {
+        const string Iso8601FormatIdentifier = "o";
+        public static string ToIso8601String(this DateTimeOffset dateTimeOffset) => dateTimeOffset.ToString(Iso8601FormatIdentifier);
+        public static string ToIso8601String(this DateOnly date) => date.ToString(Iso8601FormatIdentifier);
+    }
+}
