25cacb30d7656fddd49a5b4ff35457f04763a932
nijmra <armijn@gmail.com>
2025-08-27 13:50:50 +0200

    cleanup

--- ODBP.Server/Apis/Search/SearchRequest.cs
+++ ODBP.Server/Apis/Search/SearchRequest.cs
@@ -6,8 +6,6 @@
         public int? Page { get; init; }
         public int? PageSize { get; init; }
         public string? Sort { get; init; }
-        public DateTimeOffset? RegistratiedatumVanaf { get; init; }
-        public DateTimeOffset? RegistratiedatumTot { get; init; }
         public DateTimeOffset? GepubliceerdOpVanaf { get; init; }
         public DateTimeOffset? GepubliceerdOpTot { get; init; }
         public DateTimeOffset? LaatstGewijzigdDatumVanaf { get; init; }
