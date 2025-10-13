07b90f810758e20ad22396a912330b83a56cf12b
Felix Cornelissen <felix.cornelissen@icatt.nl>
2025-10-13 10:35:31 +0200

    remove unused fields from api response

--- ODBP.Server/Features/Documenten/PublicatieDocument.cs
+++ ODBP.Server/Features/Documenten/PublicatieDocument.cs
@@ -7,24 +7,15 @@
         public required string OfficieleTitel { get; set; }
         public string? VerkorteTitel { get; set; }
         public string? Omschrijving { get; set; }
-        public Eigenaar? Eigenaar { get; set; }
         public string? Publicatiestatus { get; set; }
         public DateOnly Creatiedatum { get; set; }
-        public DateTimeOffset Registratiedatum { get; set; }
         public DateTimeOffset? GepubliceerdOp { get; init; }
         public DateTimeOffset LaatstGewijzigdDatum { get; set; }
         public DateTimeOffset? Ontvangstdatum { get; set; }
         public DateTimeOffset? DatumOndertekend { get; set; }
         public required string Bestandsnaam { get; set; }
-        public required string Bestandsformaat { get; set; }
         public required double Bestandsomvang { get; set; }
         public List<Identifier>? Kenmerken { get; set; }
-    }
-
-    public class Eigenaar
-    {
-        public string? Identifier { get; set; }
-        public string? WeergaveNaam { get; set; }
     }
 
     public class Identifier
--- odbp.client/src/features/publicatie/types.ts
+++ odbp.client/src/features/publicatie/types.ts
@@ -7,13 +7,9 @@ export const PublicatieStatus = Object.freeze({
 export type Publicatie = {
   uuid?: string;
   publisher: string;
-  verantwoordelijke: string;
   officieleTitel: string;
   verkorteTitel: string;
   omschrijving: string;
-  eigenaar?: Eigenaar;
-  publicatiestatus: keyof typeof PublicatieStatus;
-  registratiedatum?: string;
   gepubliceerdOp?: string;
   laatstGewijzigdDatum?: string;
   datumBeginGeldigheid?: string | null;
@@ -29,22 +25,14 @@ export type PublicatieDocument = {
   officieleTitel: string;
   verkorteTitel?: string;
   omschrijving?: string;
-  publicatiestatus: keyof typeof PublicatieStatus;
   creatiedatum: string;
-  registratiedatum?: string;
   gepubliceerdOp?: string;
   laatstGewijzigdDatum?: string;
   ontvangstdatum?: string | null;
   datumOndertekend?: string | null;
   bestandsnaam: string;
-  bestandsformaat: string;
   bestandsomvang: number;
   kenmerken: Kenmerk[];
-};
-
-type Eigenaar = {
-  identifier: string;
-  weergaveNaam: string;
 };
 
 type Kenmerk = {
