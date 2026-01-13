41b813020fc518ee071fb767ded98b311eea0142
nijmra <armijn@gmail.com>
2026-01-13 17:00:22 +0100

    setup frontend

--- odbp.client/src/views/beheer/BeheerHomeView.vue
+++ odbp.client/src/views/beheer/BeheerHomeView.vue
@@ -1,19 +1,44 @@
 <template>
   <h1>GPP-Woo Burgerportaal (beheer)</h1>
 
-  <p>Aan deze functionaliteit wordt nog gewerkt.</p>
+  <simple-spinner v-if="isFetching" />
 
-  <p v-if="!loading">{{ data?.welcome }}</p>
+  <utrecht-alert v-else-if="error" type="error">
+    <p>Er is een fout opgetreden.</p>
+  </utrecht-alert>
+
+  <form v-else-if="homepage" class="utrecht-form" @submit.prevent="submit">
+    <utrecht-fieldset>
+      <utrecht-form-field>
+        <utrecht-form-label for="welcome">Welkomsttekst</utrecht-form-label>
+
+        <utrecht-textarea id="welcome" v-model="homepage.welcome" rows="10"></utrecht-textarea>
+      </utrecht-form-field>
+
+      <utrecht-form-field>
+        <utrecht-button type="submit" :appearance="'primary-action-button'" :disabled="isFetching"
+          >Opslaan</utrecht-button
+        >
+      </utrecht-form-field>
+    </utrecht-fieldset>
+  </form>
 </template>
 
 <script setup lang="ts">
-import { useLoader } from "@/composables/use-loader";
+import { useFetchApi } from "@/api";
+import SimpleSpinner from "@/components/SimpleSpinner.vue";
+import UtrechtAlert from "@/components/UtrechtAlert.vue";
 
-const { data, loading } = useLoader<{ welcome: string }>(() =>
-  fetch("/api/beheer/homepage", { headers: { "is-api": "true" } }).then((r) =>
-    r.ok ? r.json() : null
-  )
-);
+const {
+  data: homepage,
+  isFetching,
+  error,
+  put
+} = useFetchApi(() => "/api/beheer/homepage").json<{
+  welcome: string;
+}>();
+
+const submit = async () => await put(homepage).execute();
 </script>
 
 <style lang="scss" scoped></style>
