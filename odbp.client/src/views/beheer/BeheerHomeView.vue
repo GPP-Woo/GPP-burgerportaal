4efae177f7af9b3286d1fa3960c842327884c30c
nijmra <armijn@gmail.com>
2026-01-15 17:05:59 +0100

    popover

--- odbp.client/src/views/beheer/BeheerHomeView.vue
+++ odbp.client/src/views/beheer/BeheerHomeView.vue
@@ -16,9 +16,40 @@
       </utrecht-form-field>
 
       <utrecht-form-field>
-        <utrecht-form-label for="videoUrl">Promotie- of instructievideo</utrecht-form-label>
+        <utrecht-form-label for="videoUrl">
+          Promotie- of instructievideo
 
-        <utrecht-textbox id="videoUrl" v-model="homepage.videoUrl" />
+          <gpp-woo-info-popover>
+            <template #trigger="{ triggerProps }">
+              <utrecht-button
+                v-bind="triggerProps"
+                appearance="primary-action-button"
+                class="gpp-woo-info-popover__trigger"
+                >?</utrecht-button
+              >
+            </template>
+
+            <utrecht-paragraph class="gpp-woo-info-popover__content gpp-woo-pre-wrap"
+              >De video wordt op de homepage naast de welkomsttekst getoond. De waarde moet een
+              YouTube of Vimeo embed URL zijn, bijvoorbeeld
+              <em>https://www.youtube.com/embed/dQw4w9WgXcQ</em> of
+              <em>https://player.vimeo.com/video/123456789</em>.
+            </utrecht-paragraph>
+
+            <utrecht-paragraph class="gpp-woo-info-popover__content gpp-woo-pre-wrap"
+              ><strong>Let op:</strong> Cross-Origin-Embedder-Policy wordt uitgeschakeld wanneer
+              deze variabele is ingesteld. De embedder is verantwoordelijk voor juiste
+              toegankelijkheid van video content, inclusief ondertiteling en toetsenbordnavigatie.
+            </utrecht-paragraph>
+          </gpp-woo-info-popover>
+        </utrecht-form-label>
+
+        <utrecht-textbox
+          id="videoUrl"
+          v-model="homepage.videoUrl"
+          placeholder="https://"
+          :pattern="videoUrlPattern"
+        />
       </utrecht-form-field>
 
       <utrecht-form-field class="form-actions">
@@ -42,9 +73,13 @@
 import { useId } from "vue";
 import { useCloned } from "@vueuse/core";
 import { useFetchApi } from "@/api";
+import CkEditor from "@/components/ckeditor";
 import SimpleSpinner from "@/components/SimpleSpinner.vue";
 import UtrechtAlert from "@/components/UtrechtAlert.vue";
-import CkEditor from "@/components/ckeditor";
+import GppWooInfoPopover from "@/components/GppWooInfoPopover.vue";
+
+const videoUrlPattern =
+  "https://((www\\.)?(youtube\\.com|youtube-nocookie\\.com)/embed/[\\w\\-]+|player\\.vimeo\\.com/video/\\d+).*";
 
 type HomepageBeheer = { welcome: string; videoUrl: string };
 
@@ -56,7 +91,11 @@ const { data, isFetching, error, put } = useFetchApi(
 
 const { cloned: homepage, isModified, sync } = useCloned(data);
 
-const submit = async () => await put(homepage).execute();
+const submit = async () => {
+  if (homepage.value?.videoUrl !== data.value?.videoUrl) console.log('validate video');
+  
+  await put(homepage).execute();
+};
 </script>
 
 <style lang="scss" scoped>
@@ -73,5 +112,24 @@ const submit = async () => await put(homepage).execute();
     display: flex;
     justify-content: space-between;
   }
+}
+
+.gpp-woo-info-popover__trigger {
+  --utrecht-button-min-block-size: var(--gpp-woo-popover-trigger-button-size);
+  --utrecht-button-min-inline-size: var(--gpp-woo-popover-trigger-button-size);
+  --utrecht-button-inline-size: var(--gpp-woo-popover-trigger-button-size);
+
+  font-size: var(--gpp-woo-popover-trigger-font-size);
+  block-size: var(--gpp-woo-popover-trigger-button-size);
+  padding: 0;
+  border: none;
+  border-radius: 50%;
+  vertical-align: top;
+}
+
+.gpp-woo-info-popover__content {
+  --utrecht-paragraph-margin-block-start: 0;
+
+  cursor: text;
 }
 </style>
