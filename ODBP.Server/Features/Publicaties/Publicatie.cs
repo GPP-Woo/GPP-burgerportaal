3357220ac99655be391ffe73c2c1023ee2df4a49
nijmra <armijn@gmail.com>
2026-09-23 13:25:44 +0200

    add inzageprocedure model to publicatie

--- ODBP.Server/Features/Publicaties/Publicatie.cs
+++ ODBP.Server/Features/Publicaties/Publicatie.cs
@@ -15,11 +15,23 @@
         public List<string>? InformatieCategorieen { get; set; }
         public List<string>? Onderwerpen { get; set; }
         public List<Identifier>? Kenmerken { get; set; }
+        public InzageProcedure? InzageProcedure { get; set; }
     }
 
     public class Identifier
     {
         public string? Kenmerk { get; set; }
         public string? Bron { get; set; }
+    }
+
+    public class InzageProcedure
+    {
+        public Guid Uuid { get; set; }
+        public string? UrlBekendmaking { get; set; }
+        public string? Toelichting { get; set; }
+        public string? BeschikbaarRechtsmiddel { get; set; }
+        public string? UrlReactieformulier { get; set; }
+        public DateOnly? DatumBeginInzagetermijn { get; set; }
+        public DateOnly? DatumEindeInzagetermijn { get; set; }
     }
 }
